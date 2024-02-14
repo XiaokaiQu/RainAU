9139b8e2be3d43b38589c354e37b5c50cfb1b240
XiaokaiQu <18738128755@163.com>
2024-02-14 19:54:47 +0800

    configure gunicorn

--- rain_au/urls.py
+++ rain_au/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
 
 
 urlpatterns = [
     path('rainAU/', include('rainAU.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
