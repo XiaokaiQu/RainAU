0e581d4fa966698a1aa1d5f1c9b881534cdf0476
XiaokaiQu <18738128755@163.com>
2024-02-15 22:02:09 +0800

    add allowed host

--- rain_au/settings.py
+++ rain_au/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','3.107.12.90']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','3.107.12.90','hiwaperth.com']
 
 
 # Application definition
